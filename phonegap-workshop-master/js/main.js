e1ae9a528236c0ec8aff9db2a646ea8d4b7baa6d
Kyle <aroiths@yahoo.com>
2013-03-13 15:54:04 -0500

    Revert "part 5"
    This reverts commit 167e7a7e1f84c2d5ba230f976d64296399d78675.

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -1,17 +1,21 @@
 var app = {
 
-findByName: function() {
-    var self = this;
-    this.store.findByName($('.search-key').val(), function(employees) {
-        $('.employee-list').html(self.employeeLiTpl(employees));
-    });
-},
+    findByName: function() {
+        console.log('findByName');
+        this.store.findByName($('.search-key').val(), function(employees) {
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
+        });
+    },
 
      initialize: function() {
          this.store = new MemoryStore();
          $('.search-key').on('keyup', $.proxy(this.findByName, this));
-		 this.homeTpl = Handlebars.compile($("#home-tpl").html());
-         this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
      },
 	
 	initialize: function() {
@@ -38,7 +42,13 @@ initialize: function() {
 },
 
 renderHomeView: function() {
-    $('body').html(this.homeTpl());
+    var html =
+            "<div class='header'><h1>Home</h1></div>" +
+            "<div class='search-view'>" +
+            "<input class='search-key'/>" +
+            "<ul class='employee-list'></ul>" +
+            "</div>"
+    $('body').html(html);
     $('.search-key').on('keyup', $.proxy(this.findByName, this));
 },
 	
